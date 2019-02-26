3d2dfa1a373f7ec2d4a3d7cf1a2f0a1b87b0d973
Virginia <virginialash1@gmail.com>
2019-02-26 16:38:27 -0500

    modified routes/indexjs and indexjs

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-module.exports = router;
+router.get('/', (req, res) => {
+	res.render('index');
+});
 
+module.exports = router;
