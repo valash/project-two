58265f7820be39f0e1384878face702bdb0c6e41
Virginia <virginialash1@gmail.com>
2019-02-27 11:12:26 -0500

    trying to make the login and signup work by adding router stuff to user

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,6 @@ router.get('/', (req, res) => {
 
 // adding the item router to this Master router
 router.use('/item', require('./item'));
+router.use('/user', require('./user'));
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,10 @@
-router.get('/login', userController.getLogin);
-router.post('/login', userController.postLogin);
-router.get('/logout', userController.getLogout);
+const express = require('express');
+
+const router = express.Router();
+const userController = require('../controllers/user');
+
+router.get('/user/login', userController.getLogin);
+router.post('/user/login', userController.postLogin);
+router.get('/user/logout', userController.getLogout);
 
 module.exports = router;
