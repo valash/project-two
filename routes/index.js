611a47a0f9a5acffe4c1d0638c49750e7f56f1ae
Virginia <virginialash1@gmail.com>
2019-02-27 14:10:35 -0500

    routes/index update

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const { Item } = require('../models/index');
+const authenticatedUser = require('../utils/authenticaedUser');
 
-router.get('/', (req, res) => {
-	res.render('index');
+router.get('/', authenticatedUser, function(req, res) {
+	Item.find({}).sort({ name: 'asc' }).then((items) => {
+		res.render('index', { items, success: req.flash('sucess') });
+	});
 });
 
 // adding the item router to this Master router
 router.use('/item', require('./item'));
 router.use('/user', require('./user'));
+
+router.use(require('./user'));
+router.use(require('./item'));
 
 module.exports = router;
