86f1f3ca77df61e2b082a0389dec7e9548d1960b
Virginia <virginialash1@gmail.com>
2019-02-27 13:31:46 -0500

    added some dependencies i was missing

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+// const express = require('express');
+// const app = express();
+// const bodyParser = require('body-parser');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set('view engine', 'hbs');
+// app.set('view engine', 'hbs');
 
-app.use(require('./routes/index'));
+// app.use(require('./routes/index'));
 
-app.listen(3005, () => console.log('LETS GO'));
+// app.listen(3005, () => console.log('LETS GO'));
