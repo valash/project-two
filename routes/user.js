bde9e4d7810905a6ef471017ae553cfaae84d454
Virginia <virginialash1@gmail.com>
2019-02-27 13:24:22 -0500

    trying different way of doing routers in item

--- routes/user.js
+++ routes/user.js
@@ -3,8 +3,11 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user');
 
-router.get('/user/login', userController.getLogin);
-router.post('/user/login', userController.postLogin);
-router.get('/user/logout', userController.getLogout);
+router.get('/login', userController.getLogin);
+router.post('/login', userController.postLogin);
+router.get('/logout', userController.getLogout);
+
+router.get('/signup', userController.getSignup);
+router.post('/signup', userController.postSignup);
 
 module.exports = router;
