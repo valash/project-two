31fcef5da5c9273861d68cc78fe1b7001f67c1d6
Virginia <virginialash1@gmail.com>
2019-02-27 13:52:54 -0500

    the SIGNUP PAGE IS RENDING THINGS YASSSSSSSSS

--- models/user.js
+++ models/user.js
@@ -1,9 +1,12 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const UserSchema = new Schema({
-    username: String,
-    password: String
+	username: String,
+	password: String
 });
+
+UserSchema.plugin(passportLocalMongoose);
 
 module.exports = UserSchema;
--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,14 @@
 const express = require('express');
-
 const router = express.Router();
 const userController = require('../controllers/user');
 
-router.get('/login', userController.getLogin);
-router.post('/login', userController.postLogin);
-router.get('/logout', userController.getLogout);
-
 router.get('/signup', userController.getSignup);
-router.post('/signup', userController.postSignup);
+
+// router.get('/login', userController.getLogin);
+// router.post('/login', userController.postLogin);
+// router.get('/logout', userController.getLogout);
+
+// router.get('/signup', userController.getSignup);
+// router.post('/signup', userController.postSignup);
 
 module.exports = router;
