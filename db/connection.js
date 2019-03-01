46e5683412a81ddd3395eccffd05d71fcaae0c5f
Virginia <virginialash1@gmail.com>
2019-03-01 10:38:54 -0500

    update connection

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/manage');
-
 if (process.env.NODE_ENV == 'production') {
 	mongoose.connect(process.env.DB_URL);
 } else {
