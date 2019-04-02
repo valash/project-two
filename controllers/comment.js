dbe7e7ed8e6fa0d0f922c57da4fe2d95d98d301c
Virginia Lash <virginialash1@gmail.com>
2019-04-02 11:21:28 -0400

    addding crud to comments

--- controllers/comment.js
+++ controllers/comment.js
@@ -1,24 +1,52 @@
-// const { Comment } = require('../models/index');
-// const { Item } = require('../models/item');
+const { Comment } = require('../models/index');
 
-// module.exports = {
-//     new: function(req, res) {
-//         console.log('in');
-//         res.render(`comment/new`);
-//     },
-//     create: function(req, res) {
-//         const { comment } = req.body;
-//         Comment.create({
-//             comment,
-//             owner: req.user._id
-//         }).then(comment => {
-//             res.redirect(`item/${item._id}/comment/new`, { comment });
-//         });
-//     },
-//     delete: function(req, res) {
-//         Comment.deleteOne({ _id: req.params.id }).then(comment => {
-//             // console.log(item);
-//             res.redirect('/');
-//         });
-//     }
-// };
+module.exports = {
+	new: function(req, res) {
+		res.render('comment/new');
+	},
+	create: function(req, res) {
+		const { comment } = req.body;
+		Comment.create({
+			comment,
+			owner: req.user._id
+		}).then((comment) => {
+			res.redirect(`comment/${comment._id}`);
+		});
+	},
+	show: function(req, res) {
+		Comment.findById(req.params.id).then((comment) => {
+			return res.render('comment/show', { comment });
+		});
+	},
+	edit: function(req, res) {
+		Comment.findById(req.params.id).then((comment) => {
+			res.render('comment/edit', { comment });
+		});
+	},
+	update: function(req, res) {
+		const { comment } = req.body;
+
+		Comment.findByIdAndUpdate(
+			req.params.id,
+			{
+				comment
+			},
+			{
+				runValidators: true
+			}
+		)
+			.then((comment) => {
+				res.redirect(`/comment/${comment._id}`);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	},
+	delete: function(req, res) {
+		Comment.deleteOne({ _id: req.params.id }).then(
+			(comment) => {
+				res.redirect('/');
+			}
+		);
+	}
+};
